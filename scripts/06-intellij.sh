89130305d9cac2762ea3925178d8ee924e509653
Ben Chuanlong Du <dclong@users.noreply.github.com>
2018-09-16 00:53:37 +0800

    Update 06-intellij.sh

--- scripts/06-intellij.sh
+++ scripts/06-intellij.sh
@@ -2,7 +2,7 @@
 
 if [[ ! -d $HOME/.IdeaIC2018.2 ]]; then
     mv /settings/IdeaIC2018.2 $HOME/.IdeaIC2018.2
-    chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.IdeaIC2017.2
+    chown -R $DOCKER_USER:$DOCKER_GROUP $HOME/.IdeaIC2018.2
 fi
 
 if [[ ! -f $HOME/.m2/settings.xml ]]; then
